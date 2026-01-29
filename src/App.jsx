06c9d33b12c6f4a0bd703eba90ef844b9adf5b87
Mon Jearanai <mxnmcx@gmail.com>
2026-01-29 20:19:51 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import ProfileImg from './assets/images/mon01.png';
+import TypeImg from './assets/images/logo02.png';
+import TwImg from './assets/images/logo03.png';
+import SpbImg from './assets/images/logo04.png';
+import RaImg from './assets/images/logo05.png';
+import NodeImg from './assets/images/logo06.png';
+import NxImg from './assets/images/logo07.png';
+import JsImg from './assets/images/logo08.png';
+import HtmlImg from './assets/images/logo09.png';
+import GithImg from './assets/images/logo10.png';
+import GitImg from './assets/images/logo11.png';
+
+
+function App() {
+  return <img src={ProfileImg} alt="Profile Image" />;
+  <img src={TypeImg} alt="TypeScript Logo" />;
+  <img src={TwImg} alt="Tailwind Logo" />;
+  <img src={SpbImg} alt="Supabase Logo" />;
+  <img src={RaImg} alt="React Logo" />;
+  <img src={NodeImg} alt="Node Logo" />;
+  <img src={NxImg} alt="Next Logo" />;
+  <img src={JsImg} alt="JavaScript Logo" />;
+  <img src={HtmlImg} alt="HTML Logo" />;
+  <img src={GithImg} alt="GitHub Logo" />;
+  <img src={GitImg} alt="Git Logo" />;
+}
+
+
+export default App
